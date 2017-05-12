510f3b42686028fc2ae57816885a379d1593e827
jch <274458208@qq.com>
2017-05-12 15:50:52 +0800

    分开文件以便域名解析，修改完善交互及内容

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(function() {
-    var doc = $(window);
-    var artListUl = $(".artList>ul");
-    var recommendUl = $(".recommend>ul");
-    var details = $('.article_detail');
-    var art_head = $('.article_head');
-    var menu_tab = $('.menu_con');
+    var doc = $(window),
+        header = $('#header'), //主要是针对移动端，注意PC也有headid
+        artListUl = $(".artList>ul"),
+        recommendUl = $(".recommend>ul"),
+        mRecommendUl = $(".m_recommend>ul"),
+        mArtList = $(".m_artList>ul"),
+        mDetails = $(".m_details"),
+        details = $('.article_detail'),
+        art_head = $('.article_head'),
+        menu_tab = $('.menu_con'),
+        mWeChat = $('#m_weChat'),
+        mask = $('#mask'),
+        arrow = $(".arrow");
     var delay = 60 * 60 * 3 * 1000; //3小时缓存机制
     var cache = { aL: 'artList', aLT: 'artList_time' };
     var util = {
@@ -30,15 +37,14 @@ $(function() {
     var url = {
         al: 'https://app.enclavebooks.cn/v1_4/index', //article list
         ad: 'http://app.enclavebooks.cn/v1_4/article?', //article detail
-        ar: 'http://app.enclavebooks.cn/v1_4/recommend' //article random  随机三篇
+        ar: 'http://app.enclavebooks.cn/v1_4/recommend' ,//article random  随机三篇
+        homepage: 'http://app.enclavebooks.cn' //article random  随机三篇
     };
-
-    /**
-     * 
-     * @return {[type]} [description]
-     */
-
-
+    //首页slides变量
+    var index = 0,
+        imgList = $('.imgcls'),
+        n = imgList.length,
+        timeDelay = 5000;
     /**
      * [docRefresh 绑定首页按F5刷新事件]
      * @return {[type]} [description]
@@ -86,27 +92,27 @@ $(function() {
                 $.each(data, function(index, item) {
                     var str = '<li>' +
                         '<a href="article.html?art_id=' + item.art_id + '" ><img src=' + item.art_thumb + ' alt=""></a>' +
-                        '<h3>' + item.art_title + '</h3>' +
-                        '<p class="des">' + item.art_description + '</p>' +
+                        '<a href="article.html?art_id=' + item.art_id + '" ><h3>' + item.art_title + '</h3></a>' +
+                        '<a href="article.html?art_id=' + item.art_id + '" ><p class="des">' + item.art_description + '</p></a>' +
                         '<div class="editor">' +
-                        '<p><span class="mark">生活岛屿</span>' + item.art_editor + '</p>' +
+                        '<p><span class="mark">' + item.cate_name + '</span>' + item.art_editor + '</p>' +
                         '</div>' +
                         '</li>';
                     artListUl.append($(str));
                 })
                 break;
-            case details://文章详情的Dom
+            case details: //文章详情的Dom
                 var str = '<img src="' + data.art_thumb + '" alt=""><h1>' + data.art_title + '</h1>' +
                     '<div class="author">' +
                     '<img src="' + data.editor_avatar + '" alt=""><span>' + data.art_editor + ' · ' + data.art_time + '</span>' +
-                    '</div>' + data.art_content +
+                    '</div>' + data.art_content.replace(/\/ueditor\/php/g,(url.homepage+"/ueditor/php")) +
                     '<div class="readOther">' +
-                    '<span class="left mark"> 生活岛屿 </span>' +
+                    '<span class="left mark">' + data.cate_name + '</span>' +
                     '<span class="reads"><i></i>' + data.art_view + '</span>' +
                     '</div>';
                 details.html($(str));
                 break;
-            case recommendUl://文章详情页下面的随机三篇文章的Dom
+            case recommendUl: //文章详情页下面的随机三篇文章的Dom
                 recommendUl.html('');
                 $.each(data, function(index, item) {
                     var str = '<li>' +
@@ -114,13 +120,50 @@ $(function() {
                         '<div class="summary">' +
                         '<h2>' + item.art_title + '</h2>' +
                         '<p>' + item.art_description + '</p>' +
-                        '<div><p><span class="mark">生活岛屿</span>' + item.art_editor + '</p></div>' +
+                        '<div><p><span class="mark">' + item.cate_name + '</span>' + item.art_editor + '</p></div>' +
                         '</div>' +
                         '</li>';
                     recommendUl.append($(str));
                 })
                 break;
+            case mRecommendUl: //移动端文章详情页下面的随机三篇文章的Dom
+                mRecommendUl.html('');
+                $.each(data, function(index, item) {
+                    var str = '<li>' +
+                        '<a href="article.html?art_id=' + item.art_id + '" ><img src=' + item.art_thumb + ' alt=""></a>' +
+                        '<div>' +
+                        '<h2>' + item.art_title + '</h2>' +
+                        '<p>' + item.art_description + '</p>' +
+                        '</li>';
+                    mRecommendUl.append($(str));
+                })
+                break;
+            case mArtList: //移动端首页列表
+                mArtList.html('');
+                $.each(data, function(index, item) {
+                    var str = '<li>' +
+                        '<a href="article.html?art_id=' + item.art_id + '" ><img src=' + item.art_thumb + ' alt=""></a>' +
+                        '<div>' +
+                        '<h3>' + item.art_title + '</h3>' +
+                        '<p>' + item.art_description + '</p>' +
+                        '</div>' +
+                        '</li>';
+                    mArtList.append($(str));
+                })
+                break;
+            case mDetails: //移动端文章详情
+                var str = '<img src="' + data.art_thumb + '" alt=""><h1>' + data.art_title + '</h1>' +
+                    '<div class="author">' +
+                    '<span>' + data.art_editor + ' · ' + data.art_time + '</span>' +
+                    '</div>' + data.art_content.replace(/\/ueditor\/php/g,(url.homepage+"/ueditor/php")) +
+                    '<div class="readOther">' +
+                    '<span class="left mark">' + data.cate_name + '</span>' +
+                    '<span class="reads"><i></i>' + data.art_view + '</span>' +
+                    '</div>';
+                mDetails.html($(str));
+                break;
         }
+
 
 
     }
@@ -166,28 +209,29 @@ $(function() {
      * [getArticleCon 得到单篇文章内容]
      * @return {[type]} [description]
      */
-    function getArticleCon() {
+    function getArticleCon(isPc) {
         var localId = location.search.substring(1); //"art_id=270"
         $.get(url.ad + localId, function(data) {
-            //console.log(data.message);
-            if (data.code == 200) {
-                //console.log(data.result);
-                var item = data.result;
-                renderDom(details, item);
-            }
-        })
-        //随机文章3篇
-        getRandomArticle();
+                //console.log(data.message);
+                if (data.code == 200) {
+                    //console.log(data.result);
+                    var item = data.result;
+                    isPc ? renderDom(details, item) : renderDom(mDetails, item);
+                }
+            })
+            //随机文章3篇
+        getRandomArticle(isPc);
     }
 
-    (details.length != 0) && getArticleCon();
+    (details.length != 0) && getArticleCon(true);
+    (mDetails.length != 0) && getArticleCon(false);
 
-    function getRandomArticle() {
+    function getRandomArticle(isPc) {
         $.get(url.ar, function(data) {
-            //console.log(data);
+            // console.log(data);
             if (data.code == 200) {
                 var res = data.result.data;
-                renderDom(recommendUl, res);
+                isPc ? renderDom(recommendUl, res) : renderDom(mRecommendUl, res);
             }
         })
     }
@@ -226,6 +270,129 @@ $(function() {
                 menu_tab.eq(index).show();
             })
         }
+        location.search && switchIndexTab(tab,menu_tab);
     }
+    
+
+    function switchIndexTab(tab,menu_tab){
+        var index = location.search.split("=")[1];
+        tab.removeAttr('class');
+        $(tab[index]).addClass('curr');
+        menu_tab.hide();
+        menu_tab.eq(index).show();
+    }
+    
     (menu_tab.length != 0) && tabChange();
+    /*******************************移动端交互***********************************/
+    /**
+     * [resizeLoad 自适应全屏]
+     * @return {[type]} [description]
+     */
+    function resizeLoad() {
+        var self = this;
+        var width = doc.width();
+        var winWidth = width;
+        var winHeight = doc.height();
+        header.css({
+            width: winWidth,
+            height: winHeight
+        })
+
+    }
+    header && resizeLoad();
+
+    /**
+     * [scrollTop 向上滚动一屏]
+     * @return {[type]} [description]
+     */
+    function scrollTop() {
+        arrow.on('click', function() {
+            $('body,html').animate({ scrollTop: doc.height() }, 400)
+        })
+    }
+    arrow && scrollTop();
+    /**
+     * [getMArtList 获取移动端文章列表]
+     * @return {[type]} [description]
+     */
+    function getMArtList() {
+        $('.m_artList').addClass('loadingBg');
+        $.get(url.al, function(data) {
+            // console.log(data);
+            if (data.code == 200) {
+                var artArray = data.result.data;
+                artArray.length = 6;
+                renderDom(mArtList, artArray);
+                $('.m_artList').removeClass('loadingBg');
+            }
+        })
+    }
+
+    (mArtList.length != 0) && getMArtList();
+    /* 首页slides     */
+    if (n != 0) {
+        window.setInterval(function() {
+            (index == n - 1) ? (index = 0) : index++;
+            imgList.removeClass('active').removeClass('before');
+            imgList.eq(index).addClass('active');
+            if (index == 0) {
+                imgList.eq(n - 1).addClass('before');
+            } else {
+                imgList.eq(index - 1).addClass('before');
+            }
+        }, timeDelay)
+    }
+
+    //移动端首页的遮罩二维码
+    function showMask() {
+            mask.css({
+                top: doc.scrollTop(),
+                height: window.innerHeight
+                    //$(window).height()  safari有BUG
+            })
+            mask.fadeIn(100);
+            doc.on("touchmove", function(e){
+                e.preventDefault();
+            });
+        }
+    function hideMask() {
+            mask.fadeOut(100);
+            doc.off("touchmove", function(e){
+                e.preventDefault();
+            });
+    }
+    mWeChat.on('click',function(){
+        showMask();
+    })
+    mask.on("click", function(){
+        hideMask()
+    });
+
+
+
+
+    //封装移动端Touch事件
+    function tapEvt(dom, callBack) {
+        //第一不能超过延时时间，第二不能使移动  
+        //获取一些必要的值开始时间，延时时间，是否是移动  
+        var startTime = 0;
+        var delayTime = 200;
+        var isMove = false;
+        dom.addEventListener("touchstart", function(event) {
+            //记录开始时间  
+            startTime = Date.now();
+        });
+        dom.addEventListener("touchmove", function(event) {
+            //如果发生了移动就改变isMove的值  
+            isMove = true;
+        });
+        dom.addEventListener("touchend", function(event) {
+            //如果发生了移动就不执行回调  
+            if (isMove) return;
+            //如果大于延时时间就不执行回调函数  
+            if (Date.now() - startTime > delayTime) return;
+            callBack(event);
+        });
+    }
+
 });
